d44a7dd947c39d87bd9b254b53cfe9636a6ba0b7
Tristan Marcus <tjm429@nau.edu>
2020-11-13 20:01:57 -0700

    Updated App.js

--- badge/src/App.js
+++ badge/src/App.js
@@ -1,12 +1,18 @@
+import React, { Component } from "react";
+import firebase from "firebase"
 import './App.css';
-import firebase from "./firebase"
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {};
+  }
+
+  render() {
+      return (
+          <div>
+          </div>
+      );
+  }
 }
-
 export default App;
