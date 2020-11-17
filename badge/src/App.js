9653a4e37204da93a924373242d2bcdf174260cd
Tristan Marcus <tjm429@nau.edu>
2020-11-17 01:50:25 -0700

    Added messaging capabilities with firebase integration

--- badge/src/App.js
+++ badge/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { useState } from "react";
 import './App.css';
 
 import Homepage from "./Components/Homepage";
@@ -31,7 +32,7 @@ function App() {
     return (
         <div>
             <section>
-                {user ? <Homepage /> : <SignIn />}
+                {user ? <ChatRoom /> : <SignIn />}
             </section>
         </div>
     );
@@ -52,6 +53,59 @@ function SignIn() {
 function SignOut() {
     return auth.currentUser && (
         <button onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
+
+function ChatRoom() {
+    const messagesRef = firestore.collection('messages');
+
+    const query = messagesRef.orderBy('createdAt').limit(25);
+
+    const [messages] = useCollectionData(query, {idField: 'id'});
+
+    const [formValue, setFormValue] = useState('');
+
+    const sendMessage = async(e) => {
+        e.preventDefault();
+        const { uid, photoURL } = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        });
+    }
+
+    return (
+        <>
+        <div>
+            {messages && messages.map(msg => <RewardMessage key={msg.id} message={msg} />)}
+        </div>
+        <form onSubmit={sendMessage}>
+
+            <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+            <button type="submit">SUBMIT IT</button>
+
+        </form>
+
+        </>
+    )
+}
+
+function RewardMessage(props) {
+    const{ text, uid, photoURL} = props.message;
+
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+    
+    return (
+        <div classname={`message ${messageClass}`}>
+            <img src={photoURL} />
+            <p>
+                {text}
+            </p>
+        </div>
+            
     )
 }
 
