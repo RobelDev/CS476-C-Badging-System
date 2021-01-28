5f521372e906bf62c07bf8e11bc15fd8612b9755
Tristan Marcus <tjm429@nau.edu>
2021-01-27 19:47:41 -0700

    Minor code revision in the backend

--- badge-system-backend/server.js
+++ badge-system-backend/server.js
@@ -1,23 +1,29 @@
 const express = require("express");
-// const mongoose = require("mongoose")
 // const { connect } = require("http2");
 const connect = require("./connect");
+
+
+// Application configuration
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// Middlewares
+
 
 // connect();
-
 // connect();
-
 // app.use(morggan("dev"));
-
 // app.use(express.json());
 
-app.get("/", (req, res) => res.send("State Farm Capstone Project runinggggggg"));
+
+
+// API Endpoints
+app.get("/", (req, res) => res.status(200).send("The Server is running, This is all it displays. The Front-End is not hooked up!"));
 
 // app.use("/api/", require("./routes/"));
 // app.use("/", console.log("hello"));
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+// Listening
+app.listen(PORT, () => console.log(`Server listening on localhost: ${PORT}`));
