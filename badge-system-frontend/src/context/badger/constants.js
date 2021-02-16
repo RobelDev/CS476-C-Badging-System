b75ddf440068a494ea65bd2ba08b9c70eb7c0b2f
Robel Tegegne <robelkTegegne@gmail.com>
2021-02-15 21:33:27 -0700

    types or constants for context hooks

---
+++ badge-system-frontend/src/context/badger/constants.js
@@ -0,0 +1,7 @@
+export const LOAD_USER = 'LOAD_USER';
+export const GET_USER = 'GET_USER';
+export const LOG_IN = 'LOG_IN';
+export const CREAT_BADGE = 'CREAT_BADGE';
+export const GET_MY_BADGES = 'GET_MY_BADGES';
+export const GET_ALL_BADGES = 'GET_ALL_BADGES';
+export const LOG_OUT = 'LOG_OUT';
