63c50f3015a86bcc84a92f0c4f8815eace6d3824
Robel Tegegne <robelkTegegne@gmail.com>
2021-02-17 13:15:05 -0700

    gitignore

--- badge-system-frontend/src/App.js
+++ badge-system-frontend/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
 import Homepage from "./components/Homepage.js";
-import LogIn from "./components/LogIn"
+// import LogIn from "./components/LogIn"
 
-import logo from "./Assets/Team_Badgers_Logo.png"
+// import logo from "./Assets/Team_Badgers_Logo.png"
 
 import './App.css';
 // import BadgerState from "./context/badger/BadgerState"
