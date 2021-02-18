4335f72874408abb11444beabe0343fff29f823e
Robel Tegegne <robelkTegegne@gmail.com>
2021-02-18 15:50:40 -0700

    kudos model schema

---
+++ badge-system-backend/model/Kudos.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const kudosSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
+
+    reseaon: {
+        type: String,
+    },
+
+    kudosBank: {
+        type: Number,
+    },
+
+    kudosBank: {
+        type: Number,
+    },
+
+    
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+});
+
+module.exports = Kudos = mongoose.model("kudos", kudosSchema);
