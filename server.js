9c0268802a73aa18b9697c2b194ffa954d616962
Robel Tegegne <robelkTegegne@gmail.com>
2021-02-25 15:11:43 -0700

    fix routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const { connect } = require("http2");
 const connect = require("./connect");
+const cors = require("cors");
 
 
 // Application configuration
@@ -10,6 +11,7 @@ const app = express();
 // Middlewares
 
 app.use(express.json());
+app.use(cors());
 
 connect();
 // connect();
